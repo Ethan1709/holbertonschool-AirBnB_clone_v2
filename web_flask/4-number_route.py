655761a607c033b155e184e17795bae370615693
Ethan1709 <benyayerethan@gmail.com>
2023-04-03 12:48:44 +0200

    fixing output for n

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,6 +32,6 @@ def p(text):
 @app.route('/number/<n>', strict_slashes=False)
 def is_int(n):
     if isinstance(n, int):
-        return "{n} is a number"
+        return n + " is a number"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
