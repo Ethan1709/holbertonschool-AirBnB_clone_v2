529b727d8d68f8562f358b507ebdf8db694ad145
Ethan1709 <benyayerethan@gmail.com>
2023-04-03 12:38:04 +0200

    adding task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""hello flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>')
+def p(text):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route('/number/<n>')
+def is_int(n):
+    if type(n) is int:
+        return "n is a number"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
