4ab77899fb72612f55c93ea4714c734040e56c9b
Ethan1709 <benyayerethan@gmail.com>
2023-04-03 15:53:27 +0200

    adding task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,6 +42,6 @@ def is_int2(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def is_int3(n):
-    return render_template('5-number.html', n=n)
+    return render_template('6-number_odd_or_even.html', n=n)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
