8a933c268daa2946ac4bb253df2aff8c93dafae0
Ethan1709 <benyayerethan@gmail.com>
2023-04-03 12:21:38 +0200

    adding task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,6 +18,8 @@ def index():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    for i in text:
+        text = text.replace("_", " ")
     return "C {}".format(text)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
