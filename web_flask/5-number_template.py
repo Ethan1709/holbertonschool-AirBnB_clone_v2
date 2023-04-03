cfb8b20991fc87c41b428f490ff4f05383b87a4b
Ethan1709 <benyayerethan@gmail.com>
2023-04-03 14:03:59 +0200

    adding task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,6 +38,6 @@ def is_int(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def is_int(n):
     if type(n) is int:
-        return render_template('5-number.html')
+        return render_template('5-number.html', n=n)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
